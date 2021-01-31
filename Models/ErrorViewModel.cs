43af8586bc9bf08d550358b3fc0540ed64543260
Md. Akhtar Uzzaman <akhtaruzzaman05@gmail.com>
2021-01-31 19:25:03 +0600

    upd

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace docker_kubernetes_ci_cd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
